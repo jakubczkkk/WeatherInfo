293e7d9c6a49880e2528552f27538d387f732d59
Rafal Jakubczyk <jakubczykrafal06@gmail.com>
2021-01-13 15:08:58 +0100

    login

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, request, url_for, redirect, session
 from db import db
 
 app = Flask(__name__)
+app.secret_key = 'dsadawdw3231qw'
 
 @app.route('/')
-def main_page():
+def index():
    return render_template('main.html')
 
 @app.route('/pogoda', methods=['GET', 'POST'])
-def get_weather():
+def add_weather_info():
 
     if request.method == "GET":
         pogoda = db.weather.find()
@@ -16,7 +17,20 @@ def get_weather():
 
     if request.method == "POST":
         db.weather.insert_one(request.form.to_dict())
-        return render_template('dodano.html')
+        return redirect(url_for('index'))
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+
+    if request.method == 'GET':
+        return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    
+    session.pop('username', None)
+    return redirect(url_for('index'))
